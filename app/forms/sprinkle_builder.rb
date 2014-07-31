18f53fd84a12cc41009e19e885e95f3f74dd451b
Pier-Olivier Thibault <pothibo@gmail.com>
2014-07-30 22:26:29 -0400

    Label inconsistencies

--- app/forms/sprinkle_builder.rb
+++ app/forms/sprinkle_builder.rb
@@ -2,7 +2,7 @@ class SprinkleBuilder < ActionView::Helpers::FormBuilder
   def duration
     select :duration, @template.options_for_select([
       ["Ouverture manuelle", {disabled: true}, -1],
-      ["30 Secondes", 30],
+      ["30 secondes", 30],
       ["2 minutes", 120],
       ["5 minutes", 300]
       ], -1), {}, as: "Sprinkles.Form"
